51ea08352584ee8e066f159a858e218e6042be37
Radosław <57417179+rabranto@users.noreply.github.com>
2021-07-23 17:24:26 +0200

    First commit
    Building rectangle using "*" character.

---
+++ RectangleBulder/RectangleBulder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RectangleBulder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write wanted width: ");
+            string readWidth = Console.ReadLine();
+
+            Console.Write("Write wanted height: ");
+            string readHeight = Console.ReadLine();
+
+            int width = int.Parse(readWidth);
+            int height = int.Parse(readHeight);
+
+            Console.WriteLine();
+
+            build(width, height);
+
+            static void build(int x, int y)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("*");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
